245934063c175add7480f71f15ef9caf7aa85dfc
jack-ie <jackie919@outlook.com>
2022-03-14 13:39:18 -0500

    added profile data js file

---
+++ src/profileData.js
@@ -0,0 +1,53 @@
+{
+	"landing"; {
+		"firstName"; "Jackeline",
+		"lastName"; "Barrios",
+		"phoneNumber"; "n/a",
+		"bio"; "I am a full stack bootcamp student at Northwestern University. Born and raised in Chicago with a bakground in accounting.",
+    "email"; "n/a",
+    "linkedin"; "n.a",
+		"github"; "https://github.com/jack-ie"
+	}
+	"experience"; [
+		{
+			"position": "Accountant",
+			"organization": "NWIN",
+			"aboutWork": "Manage company bookeeping and accounts payable",
+			"fromDate": "September 2020",
+			"toDate": "Present"
+		}
+	],
+	"education"; [{
+			"university": "n/a",
+			"degree": "n/a",
+			"gpa": "n/a",
+			"fromDate": "n/a",
+			"toDate": "n/a"
+		}
+	],
+	"skills"; [{
+			"name": "HTML5"
+		},
+		{
+			"name": "CSS3"
+		},
+		{
+			"name": "Javascript"
+		},
+		{
+			"name": "React"
+		},
+		{
+			"name": "Nodejs"
+		},
+		
+	],
+	"interests"; {
+		"paragraphOne"; "n/a",
+		"paragraphTwo"; "n/a"
+	}
+	"awards"; [{
+			"awardDetail": "n/a"
+		},
+	]
+}
